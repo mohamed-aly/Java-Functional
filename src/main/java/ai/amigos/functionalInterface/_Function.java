6cf52dd115e414d442e710f22a53abfaf565deb3
mohamed-aly <mali902016@gmail.com>
2022-04-28 15:10:15 +0200

    Functions chaining

--- src/main/java/ai/amigos/functionalInterface/_Function.java
+++ src/main/java/ai/amigos/functionalInterface/_Function.java
@@ -6,11 +6,20 @@ public class _Function {
     public static void main(String[] args) {
         System.out.println(increaseByOne(5));
         System.out.println(increaseByOneFunction.apply(5));
+
+        System.out.println(increaseByOneThenMultiplyBy10Function.apply(5));
     }
 
     static Function<Integer, Integer> increaseByOneFunction = number -> ++number;
 
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    //chaining functions
+    static Function<Integer, Integer> increaseByOneThenMultiplyBy10Function =
+            increaseByOneFunction.andThen(multiplyBy10Function);
+
     static int increaseByOne(Integer number) {
         return ++number;
     }
+
 }
