29d7cd9aac32b4bb0ee5b3c849cea935e03f2a40
mohamed-aly <mali902016@gmail.com>
2022-05-09 15:12:17 +0200

    BiConsumer

--- src/main/java/ai/amigos/functionalInterface/_Consumer.java
+++ src/main/java/ai/amigos/functionalInterface/_Consumer.java
@@ -1,5 +1,6 @@
 package ai.amigos.functionalInterface;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class _Consumer {
@@ -10,9 +11,15 @@ public class _Consumer {
         //consumer takes one argument and returns nothing, like void method
         Consumer<Customer> consumer = customer ->
                 System.out.println("Hello, " + customer.name + " Thanks for registering phone number "
-                + customer.phoneNumber);
+                        + customer.phoneNumber);
 
-        consumer.accept(ahmed);
+        //BiConsumer
+        BiConsumer<Customer, Boolean> biConsumer =
+                (customer, showPhoneNumber) ->
+                        System.out.println("Hello, " + customer.name + " Thanks for registering phone number "
+                                + (showPhoneNumber ? customer.phoneNumber : "********"));
+
+        biConsumer.accept(ahmed, false);
 
     }
 
